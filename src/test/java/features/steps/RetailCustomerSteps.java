5899ceee1d9a6e21f8ee254ad436254edd521ebd
Andy Pliszka and William Ramsey <pair+andy+william@pivotallabs.com>
2013-08-08 12:11:37 -0400

    Adds feature for retail customer list

---
+++ src/test/java/features/steps/RetailCustomerSteps.java
@@ -0,0 +1,29 @@
+package features.steps;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.junit.Assert.assertTrue;
+
+public class RetailCustomerSteps {
+
+    private WebDriver driver = new HtmlUnitDriver();
+
+    @When("^I navigate to customer list page$")
+    public void I_navigate_to_customer_list_page() throws Throwable {
+        driver.get("http://localhost:8080/retailcustomers");
+    }
+
+    @Then("^I should see Alan Turing in the customer list$")
+    public void I_should_see_Alan_Turing_in_the_customer_list() throws Throwable {
+        assertTrue(driver.getPageSource().contains("Alan"));
+        assertTrue(driver.getPageSource().contains("Turing"));
+    }
+
+    @And("^there is a Alan Turing retail customer$")
+    public void there_is_a_Alan_Turing_retail_customer() throws Throwable {
+    }
+}
