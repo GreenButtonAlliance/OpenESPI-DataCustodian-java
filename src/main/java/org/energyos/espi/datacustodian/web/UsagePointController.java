76768f5be7880a9f8cc90543a48d787f56981d3d
Abhinav Shrestha and Andy Pliszka <pair+ashrestha+andy@pivotallabs.com>
2013-08-13 17:01:02 -0400

    Adds Usage Point controller

--- src/main/java/org/energyos/espi/datacustodian/web/UsagePointController.java
+++ src/main/java/org/energyos/espi/datacustodian/web/UsagePointController.java
@@ -16,16 +16,40 @@
 
 package org.energyos.espi.datacustodian.web;
 
+import org.energyos.espi.datacustodian.models.RetailCustomer;
+import org.energyos.espi.datacustodian.models.UsagePoint;
+import org.energyos.espi.datacustodian.service.UsagePointService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/usagepoints")
 public class UsagePointController {
 
+    @Autowired
+    private UsagePointService usagePointService;
+
+    @ModelAttribute
+    public List<UsagePoint> usagePoints() {
+        RetailCustomer customer = new RetailCustomer();
+        customer.setId(1L);
+
+        return usagePointService.findAllByRetailCustomer(customer);
+    }
+
     @RequestMapping(method = RequestMethod.GET)
     public String index() {
         return "usagepoints/index";
+    }
+
+    public void setUsagePointService(UsagePointService usagePointService) {
+        this.usagePointService = usagePointService;
     }
 }
---
+++ src/test/java/org/energyos/espi/datacustodian/web/UsagePointControllerTests.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2013 EnergyOS ESPI
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.energyos.espi.datacustodian.web;
+
+import org.energyos.espi.datacustodian.models.RetailCustomer;
+import org.energyos.espi.datacustodian.models.UsagePoint;
+import org.energyos.espi.datacustodian.service.UsagePointService;
+import org.energyos.espi.datacustodian.service.impl.UsagePointServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class UsagePointControllerTests {
+
+    private UsagePointController controller;
+    private UsagePointService service;
+
+    @Before
+    public void setupUp() {
+        controller = new UsagePointController();
+        service = mock(UsagePointServiceImpl.class);
+        controller.setUsagePointService(service);
+    }
+
+    @Test
+    public void index_should_displayUsagePointsPage() throws Exception {
+        assertTrue(controller.index() == "usagepoints/index");
+    }
+
+    @Test
+    public void usagePoints_should_returnUsagePointList() throws Exception {
+        List<UsagePoint> points = new ArrayList<UsagePoint>();
+        when(service.findAllByRetailCustomer(any(RetailCustomer.class))).thenReturn(points);
+
+        assertEquals(controller.usagePoints(), points);
+    }
+}
