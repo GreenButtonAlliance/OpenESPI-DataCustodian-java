c5fe5f81a1016b20dc2d2861c9afc21b1d1ab6e8
Andy Pliszka and Paul Meskers <pair+andy+pmeskers@pivotallabs.com>
2013-08-15 10:55:16 -0400

    Adds RetailCustomerService

---
+++ src/main/java/org/energyos/espi/datacustodian/service/impl/RetailCustomerServiceImpl.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2013 EnergyOS ESPI
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.energyos.espi.datacustodian.service.impl;
+
+import org.energyos.espi.datacustodian.models.RetailCustomer;
+import org.energyos.espi.datacustodian.repositories.RetailCustomerRepository;
+import org.energyos.espi.datacustodian.service.RetailCustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RetailCustomerServiceImpl implements RetailCustomerService {
+
+    @Autowired
+    private RetailCustomerRepository repository;
+
+    public void setRepository(RetailCustomerRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<RetailCustomer> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public void persist(RetailCustomer customer) {
+        repository.persist(customer);
+    }
+}
---
+++ src/test/java/org/energyos/espi/datacustodian/service/RetailCustomerServiceTests.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2013 EnergyOS ESPI
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.energyos.espi.datacustodian.service;
+
+
+import org.energyos.espi.datacustodian.models.RetailCustomer;
+import org.energyos.espi.datacustodian.repositories.RetailCustomerRepository;
+import org.energyos.espi.datacustodian.service.impl.RetailCustomerServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class RetailCustomerServiceTests {
+
+    private RetailCustomerRepository repository;
+    private RetailCustomerServiceImpl service;
+
+    @Before
+    public void before() {
+        repository = mock(RetailCustomerRepository.class);
+        service = new RetailCustomerServiceImpl();
+        service.setRepository(repository);
+    }
+
+    @Test
+    public void findAll_returnsAllRetailCustomers() {
+        List<RetailCustomer> allRetailCustomers = new ArrayList<RetailCustomer>();
+
+        when(repository.findAll()).thenReturn(allRetailCustomers);
+
+        assertEquals(allRetailCustomers, service.findAll());
+    }
+
+    @Test
+    public void persist_persistsRetailCustomer() {
+        RetailCustomer customer = new RetailCustomer();
+
+        service.persist(customer);
+
+        verify(repository).persist(customer);
+    }
+}
