5e9415c93f7c36e0568c45ca9f2f57aff654894b
Paul Meskers and William Ramsey <pair+paul+william@pivotallabs.com>
2013-08-16 10:02:54 -0400

    Changes configuration to use mysql instead of hsql

--- src/main/resources/db/mysql/initDB.sql
+++ src/main/resources/db/mysql/initDB.sql
@@ -2,16 +2,17 @@ DROP TABLE IF EXISTS usage_points;
 DROP TABLE IF EXISTS retail_customers;
 
 CREATE TABLE retail_customers (
-  id BIGINT(20) NOT NULL,
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30),
   last_name  VARCHAR(30),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE usage_points (
-  id BIGINT(20) NOT NULL,
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
   title      VARCHAR(100) NOT NULL,
-  retail_customer_id BIGINT(20) NOT NULL
+  retail_customer_id BIGINT(20) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 ALTER TABLE usage_points
