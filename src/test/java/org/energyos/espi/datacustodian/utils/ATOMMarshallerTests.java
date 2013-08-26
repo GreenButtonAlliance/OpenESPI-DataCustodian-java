0b177f3f465b73151b13d94f5deb30cc415ac5a6
Abhinav Shrestha and Andy Pliszka <pair+ashrestha+andy@pivotallabs.com>
2013-08-26 13:58:03 -0400

    Adds more fields to ATOM entry

--- src/test/java/org/energyos/espi/datacustodian/utils/ATOMMarshallerTests.java
+++ src/test/java/org/energyos/espi/datacustodian/utils/ATOMMarshallerTests.java
@@ -19,6 +19,8 @@ package org.energyos.espi.datacustodian.utils;
 import org.energyos.espi.datacustodian.models.atom.EntryType;
 import org.energyos.espi.datacustodian.models.atom.FeedType;
 import org.energyos.espi.datacustodian.models.atom.IdType;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.naesb.espi.UsagePoint;
@@ -39,6 +41,13 @@ import static junit.framework.Assert.assertEquals;
 @ContextConfiguration("/spring/test-context.xml")
 public class ATOMMarshallerTests {
 
+    String FEED_PREFIX = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+            "<?xml-stylesheet type=\"text/xsl\" href=\"GreenButtonDataStyleSheet.xslt\"?>" +
+            "<feed xmlns=\"http://www.w3.org/2005/Atom\" " +
+            " xsi:schemaLocation=\"http://naesb.org/espi espiDerived.xsd\"" +
+            " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">";
+    String FEED_POSTFIX =  "</feed>";
+
     @Autowired
     private ATOMMarshaller marshaller;
 
@@ -49,24 +58,14 @@ public class ATOMMarshallerTests {
 
     @Test
     public void givenEmptyFeed_returnsFeed() throws JAXBException {
-        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
-                     "<?xml-stylesheet type=\"text/xsl\" href=\"GreenButtonDataStyleSheet.xslt\"?>" +
-                     "<feed xmlns=\"http://www.w3.org/2005/Atom\" " +
-                     " xsi:schemaLocation=\"http://naesb.org/espi espiDerived.xsd\"" +
-                     " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"></feed>";
+        String xml = FEED_PREFIX + FEED_POSTFIX;
 
         assertEquals(FeedType.class, marshaller.unmarshal(new ByteArrayInputStream(xml.getBytes())).getClass());
     }
 
     @Test
     public void givenFeedWithEntry_returnsFeedWithEntry() throws JAXBException {
-        String xml ="<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
-                "<?xml-stylesheet type=\"text/xsl\" href=\"GreenButtonDataStyleSheet.xslt\"?>" +
-                "<feed xmlns=\"http://www.w3.org/2005/Atom\" " +
-                " xsi:schemaLocation=\"http://naesb.org/espi espiDerived.xsd\"" +
-                " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">" +
-                "  <entry></entry>" +
-                "</feed>";
+        String xml = FEED_PREFIX + "<entry></entry>" + FEED_POSTFIX;
         InputStream xmlStream = new ByteArrayInputStream(xml.getBytes());
         FeedType feed = marshaller.unmarshal(xmlStream);
 
@@ -75,17 +74,13 @@ public class ATOMMarshallerTests {
 
     @Test
     public void givenFeedWithUsagePointEntry_returnsFeedWithUsagePoint() throws JAXBException {
-        String xml ="<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
-                "<?xml-stylesheet type=\"text/xsl\" href=\"GreenButtonDataStyleSheet.xslt\"?>" +
-                "<feed xmlns=\"http://www.w3.org/2005/Atom\" " +
-                " xsi:schemaLocation=\"http://naesb.org/espi espiDerived.xsd\"" +
-                " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">" +
+        String xml = FEED_PREFIX +
                 "  <entry>" +
                 "    <content>" +
                 "      <UsagePoint xmlns=\"http://naesb.org/espi\"></UsagePoint>" +
                 "    </content>" +
                 "  </entry>" +
-                "</feed>";
+                FEED_POSTFIX;
         InputStream xmlStream = new ByteArrayInputStream(xml.getBytes());
         FeedType feed = marshaller.unmarshal(xmlStream);
 
@@ -94,17 +89,66 @@ public class ATOMMarshallerTests {
 
     @Test
     public void givenFeedWithId_returnsFeedWithId() throws JAXBException {
-        String xml ="<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
-                "<?xml-stylesheet type=\"text/xsl\" href=\"GreenButtonDataStyleSheet.xslt\"?>" +
-                "<feed xmlns=\"http://www.w3.org/2005/Atom\" " +
-                " xsi:schemaLocation=\"http://naesb.org/espi espiDerived.xsd\"" +
-                " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">" +
+        String xml = FEED_PREFIX +
                 "   <id>urn:uuid:D7B58EA6-D94D-45D1-A0CA-F8A843AB1080</id>" +
-                "</feed>";
+                FEED_POSTFIX;
         InputStream xmlStream = new ByteArrayInputStream(xml.getBytes());
         FeedType feed = marshaller.unmarshal(xmlStream);
 
         assertEquals(IdType.class, feed.getId().getClass());
 
+    }
+
+    @Test
+    public void givenEntryWithId_returnsEntryWithId() throws JAXBException {
+        String xml = FEED_PREFIX +
+                "   <entry>" +
+                "     <id>urn:uuid:D7B58EA6-D94D-45D1-A0CA-F8A843AB1080</id>" +
+                "   </entry>" +
+                FEED_POSTFIX;
+        InputStream xmlStream = new ByteArrayInputStream(xml.getBytes());
+        FeedType feed = marshaller.unmarshal(xmlStream);
+
+        assertEquals(IdType.class, feed.getEntries().get(0).getId().getClass());
+
+    }
+
+    @Test
+    public void givenEntryWithTitle_returnsEntryWithTitle() throws JAXBException {
+        String xml = FEED_PREFIX +
+                "   <entry>" +
+                "     <title>Entry Title</title>" +
+                "   </entry>" +
+                FEED_POSTFIX;
+        InputStream xmlStream = new ByteArrayInputStream(xml.getBytes());
+        FeedType feed = marshaller.unmarshal(xmlStream);
+
+        assertEquals("Entry Title", feed.getEntries().get(0).getTitle());
+    }
+
+    @Test
+    public void givenEntryWithPublished_returnsEntryWithPublished() throws JAXBException {
+        String xml = FEED_PREFIX +
+                "   <entry>" +
+                "     <published>2000-02-29T00:00:00Z</published>" +
+                "   </entry>" +
+                FEED_POSTFIX;
+        InputStream xmlStream = new ByteArrayInputStream(xml.getBytes());
+        FeedType feed = marshaller.unmarshal(xmlStream);
+
+        assertEquals(new DateTime(2000, 2, 29, 0, 0, 0, 0, DateTimeZone.UTC), feed.getEntries().get(0).getPublished());
+    }
+
+    @Test
+    public void givenEntryWithUpdate_returnsEntryWithUpdated() throws JAXBException {
+        String xml = FEED_PREFIX +
+                "   <entry>" +
+                "     <updated>2000-02-29T00:00:00Z</updated>" +
+                "   </entry>" +
+                FEED_POSTFIX;
+        InputStream xmlStream = new ByteArrayInputStream(xml.getBytes());
+        FeedType feed = marshaller.unmarshal(xmlStream);
+
+        assertEquals(new DateTime(2000, 2, 29, 0, 0, 0, 0, DateTimeZone.UTC), feed.getEntries().get(0).getUpdated());
     }
 }
