80ee1cfb5009217fc5789a17330745d8b7ed48d9
Abhinav Shrestha and Andy Pliszka <pair+ashrestha+andy@pivotallabs.com>
2013-08-26 13:58:13 -0400

    Fixes random login issues

--- src/test/java/features/steps/StepUtils.java
+++ src/test/java/features/steps/StepUtils.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2013 EnergyOS.org
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
  */
 
 package features.steps;
@@ -24,12 +24,15 @@ public class StepUtils {
 
     public static void login(String username, String password) {
         WebDriver driver = WebDriverSingleton.getInstance();
+        driver.get("http://localhost:8080/j_spring_security_logout");
         driver.get("http://localhost:8080/");
         WebElement loginLink = driver.findElement(By.id("login"));
         loginLink.click();
         WebElement usernameInput = driver.findElement(By.name("j_username"));
+        usernameInput.clear();
         usernameInput.sendKeys(username);
         WebElement passwordInput = driver.findElement(By.name("j_password"));
+        passwordInput.clear();
         passwordInput.sendKeys(password);
         WebElement login = driver.findElement(By.name("submit"));
         login.click();
