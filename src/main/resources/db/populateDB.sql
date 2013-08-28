7a09bb83f6b224118a398697966d6941a58a164e
Abhinav Shrestha and Andy Pliszka <pair+ashrestha+andy@pivotallabs.com>
2013-08-28 11:16:15 -0400

    Moves service category inserts to the top of the file

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,17 +1,3 @@
-INSERT INTO retail_customers (id, first_name, last_name) VALUES (1, 'Alan', 'Turing');
-INSERT INTO retail_customers (id, first_name, last_name) VALUES (2, 'Donald', 'Knuth');
-INSERT INTO retail_customers (id, first_name, last_name) VALUES (3, 'Paul', 'Dirac');
-INSERT INTO retail_customers (id, first_name, last_name) VALUES (4, 'Alonso', 'Church');
-INSERT INTO retail_customers (id, first_name, last_name) VALUES (5, 'Charles', 'Babbage');
-INSERT INTO retail_customers (id, first_name, last_name) VALUES (6, 'John', 'von Neumann');
-INSERT INTO retail_customers (id, first_name, last_name) VALUES (7, 'Marian', 'Rejewski');
-
-INSERT INTO usage_points (id, title, retail_customer_id) VALUES (1, 'House meter', 1);
-INSERT INTO usage_points (id, title, retail_customer_id) VALUES (2, 'Gas meter', 1);
-INSERT INTO usage_points (id, title, retail_customer_id) VALUES (3, 'House meter', 2);
-INSERT INTO usage_points (id, title, retail_customer_id) VALUES (4, 'House meter', 6);
-INSERT INTO usage_points (id, title, retail_customer_id) VALUES (5, 'Gas meter', 6);
-
 INSERT INTO service_categories (kind) VALUES (0);
 INSERT INTO service_categories (kind) VALUES (1);
 INSERT INTO service_categories (kind) VALUES (2);
@@ -22,3 +8,18 @@ INSERT INTO service_categories (kind) VALUES (6);
 INSERT INTO service_categories (kind) VALUES (7);
 INSERT INTO service_categories (kind) VALUES (8);
 INSERT INTO service_categories (kind) VALUES (9);
+
+INSERT INTO retail_customers (id, first_name, last_name) VALUES (1, 'Alan', 'Turing');
+INSERT INTO retail_customers (id, first_name, last_name) VALUES (2, 'Donald', 'Knuth');
+INSERT INTO retail_customers (id, first_name, last_name) VALUES (3, 'Paul', 'Dirac');
+INSERT INTO retail_customers (id, first_name, last_name) VALUES (4, 'Alonso', 'Church');
+INSERT INTO retail_customers (id, first_name, last_name) VALUES (5, 'Charles', 'Babbage');
+INSERT INTO retail_customers (id, first_name, last_name) VALUES (6, 'John', 'von Neumann');
+INSERT INTO retail_customers (id, first_name, last_name) VALUES (7, 'Marian', 'Rejewski');
+
+INSERT INTO usage_points (id, title, serviceCategory_kind, retail_customer_id) VALUES (1, 'House meter', 1, 1);
+INSERT INTO usage_points (id, title, serviceCategory_kind, retail_customer_id) VALUES (2, 'Gas meter', 1, 1);
+INSERT INTO usage_points (id, title, serviceCategory_kind, retail_customer_id) VALUES (3, 'House meter', 1, 2);
+INSERT INTO usage_points (id, title, serviceCategory_kind, retail_customer_id) VALUES (4, 'House meter', 1, 6);
+INSERT INTO usage_points (id, title, serviceCategory_kind, retail_customer_id) VALUES (5, 'Gas meter', 1, 6);
+
