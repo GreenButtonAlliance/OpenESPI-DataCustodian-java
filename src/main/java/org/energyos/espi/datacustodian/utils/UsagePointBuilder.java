ff1c3e97e0ea59a72b08b5752d46d181117696cc
Alex Kramer and William Ramsey <pair+akramer+wramsey@pivotallabs.com>
2013-09-05 17:57:21 -0400

    Extracts EntryLookupTable from UsagePointBuilder.

--- src/main/java/org/energyos/espi/datacustodian/utils/UsagePointBuilder.java
+++ src/main/java/org/energyos/espi/datacustodian/utils/UsagePointBuilder.java
@@ -23,13 +23,10 @@ import org.energyos.espi.datacustodian.domain.UsagePoint;
 import org.energyos.espi.datacustodian.models.atom.ContentType;
 import org.energyos.espi.datacustodian.models.atom.EntryType;
 import org.energyos.espi.datacustodian.models.atom.FeedType;
-import org.energyos.espi.datacustodian.models.atom.LinkType;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Component
 public class UsagePointBuilder {
@@ -102,63 +99,5 @@ public class UsagePointBuilder {
             }
         }
         return null;
-    }
-
-    private class EntryLookupTable {
-        private Map<String, EntryType> entryMap;
-
-        public EntryLookupTable(List<EntryType> entries) {
-            entryMap = new HashMap<>();
-
-            addSelfLinks(entries);
-            addRelatedLinks(entries);
-        }
-
-        public EntryType getUpEntry(EntryType entry) {
-            return entryMap.get(getUpLinkHref(entry));
-        }
-
-        public List<EntryType> getRelatedEntries(EntryType entryType) {
-            List<EntryType> relatedEntries = new ArrayList<>();
-            for (LinkType link : entryType.getLinks()) {
-                if (link.getRel().equals("related")) {
-                    relatedEntries.add(entryMap.get(link.getHref()));
-                }
-            }
-            return relatedEntries;
-        }
-
-        private void addSelfLinks(List<EntryType> entries) {
-            for (EntryType entry : entries) {
-                entryMap.put(getSelfLinkHref(entry), entry);
-            }
-        }
-
-        private void addRelatedLinks(List<EntryType> entries) {
-            for (EntryType entry : entries) {
-                for (LinkType link : entry.getLinks()) {
-                    if (link.getRel().equals("related") && !entryMap.containsKey(link.getHref())) {
-                        entryMap.put(link.getHref(), entry);
-                    }
-                }
-            }
-        }
-
-        private String getSelfLinkHref(EntryType entry) {
-            return getLinkHrefForType(entry, "self");
-        }
-
-        private String getUpLinkHref(EntryType entry) {
-            return getLinkHrefForType(entry, "up");
-        }
-
-        private String getLinkHrefForType(EntryType entry, String type) {
-            for (LinkType link : entry.getLinks()) {
-                if (link.getRel().equals(type)) {
-                    return link.getHref();
-                }
-            }
-            return null;
-        }
     }
 }
