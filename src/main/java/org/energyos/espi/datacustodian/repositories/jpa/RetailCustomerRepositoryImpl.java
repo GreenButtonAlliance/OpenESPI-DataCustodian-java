e4f97f34d56ba29fdfa15e6164bf2af4985ed459
Andy Pliszka <andy@isp44.com>
2013-09-10 19:43:06 -0400

    DataCustodian database security provider

--- src/main/java/org/energyos/espi/datacustodian/repositories/jpa/RetailCustomerRepositoryImpl.java
+++ src/main/java/org/energyos/espi/datacustodian/repositories/jpa/RetailCustomerRepositoryImpl.java
@@ -26,7 +26,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
-//@Repository
+@Repository
 public class RetailCustomerRepositoryImpl implements RetailCustomerRepository {
 
     @PersistenceContext
