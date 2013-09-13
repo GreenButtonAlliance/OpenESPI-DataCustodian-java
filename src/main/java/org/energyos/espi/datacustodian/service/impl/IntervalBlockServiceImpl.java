39dbe7f4940e2a668146598b6ff553bcd783f01b
Alex Kramer and Andy Pliszka <pair+akramer+andy@pivotallabs.com>
2013-09-13 14:32:10 -0400

    Enable IntervalBlockService

--- src/main/java/org/energyos/espi/datacustodian/service/impl/IntervalBlockServiceImpl.java
+++ src/main/java/org/energyos/espi/datacustodian/service/impl/IntervalBlockServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-//@Service
+@Service
 public class IntervalBlockServiceImpl implements IntervalBlockService {
 
     @Autowired
