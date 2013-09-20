1a6fc9329af8770b52e62e37e6e7f95f9743ecb3
Abhinav Shrestha and Andy Pliszka <pair+ashrestha+andy@pivotallabs.com>
2013-09-20 16:05:01 -0400

    Adds FixtureFactory

---
+++ src/test/java/org/energyos/espi/datacustodian/utils/factories/FixtureFactory.java
@@ -0,0 +1,24 @@
+package org.energyos.espi.datacustodian.utils.factories;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+public class FixtureFactory {
+    private FixtureFactory() {};
+
+    public static InputStream newUsagePointInputStream(UUID uuid) throws IOException {
+        return new ByteArrayInputStream(newUsagePointXML(uuid).getBytes());
+    }
+
+    public static String newUsagePointXML(UUID uuid) throws IOException {
+        ClassPathResource sourceFile = new ClassPathResource("/fixtures/15minLP_15Days.xml");
+        String xml = FileUtils.readFileToString(sourceFile.getFile());
+        xml = xml.replaceFirst("7BC41774-7190-4864-841C-861AC76D46C2", uuid.toString());
+        return xml;
+    }
+}
