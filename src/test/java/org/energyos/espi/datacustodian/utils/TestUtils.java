4b4685a4933572d9c3ec975a5136696c58508468
Abhinav Shrestha and Andy Pliszka <pair+ashrestha+andy@pivotallabs.com>
2013-09-20 16:05:00 -0400

    Adds TestUtils with a method to import Usage Point

---
+++ src/test/java/org/energyos/espi/datacustodian/utils/TestUtils.java
@@ -0,0 +1,18 @@
+package org.energyos.espi.datacustodian.utils;
+
+import org.energyos.espi.datacustodian.domain.RetailCustomer;
+import org.energyos.espi.datacustodian.service.UsagePointService;
+import org.energyos.espi.datacustodian.utils.factories.FixtureFactory;
+
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+import java.util.UUID;
+
+public class TestUtils {
+    private TestUtils() {};
+
+    public static void importUsagePoint(UsagePointService usagePointService, RetailCustomer customer, UUID uuid) throws JAXBException, IOException {
+        usagePointService.importUsagePoints(FixtureFactory.newUsagePointInputStream(uuid));
+        usagePointService.associateByUUID(customer, uuid);
+    }
+}
