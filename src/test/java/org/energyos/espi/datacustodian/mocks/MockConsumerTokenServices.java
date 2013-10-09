a92b1241f1b7407ef6f8a9245a26e4bb5728a5ff
Andy Pliszka and Joshua Clark <pair+apliszka+jclark@pivotallabs.com>
2013-10-09 16:03:22 -0400

    Bugfix missing dependency during test

---
+++ src/test/java/org/energyos/espi/datacustodian/mocks/MockConsumerTokenServices.java
@@ -0,0 +1,30 @@
+package org.energyos.espi.datacustodian.mocks;
+
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class MockConsumerTokenServices implements ConsumerTokenServices {
+    @Override
+    public Collection<OAuth2AccessToken> findTokensByUserName(String s) {
+        return null;
+    }
+
+    @Override
+    public Collection<OAuth2AccessToken> findTokensByClientId(String s) {
+        return null;
+    }
+
+    @Override
+    public boolean revokeToken(String s) {
+        return false;
+    }
+
+    @Override
+    public String getClientId(String s) {
+        return null;
+    }
+}
