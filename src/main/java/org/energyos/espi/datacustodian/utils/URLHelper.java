5b16ebff4cb27e252c47676e81d6ba6094a27d3b
Andy Pliszka and Joshua Clark <pair+apliszka+jclark@pivotallabs.com>
2013-10-10 14:18:25 -0400

    Adds UrlHelper class with method for scope param generation.

---
+++ src/main/java/org/energyos/espi/datacustodian/utils/URLHelper.java
@@ -0,0 +1,14 @@
+package org.energyos.espi.datacustodian.utils;
+
+public class URLHelper {
+
+    public static String newScopeParams(String[] scopes) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < scopes.length; i++) {
+            if(i > 0)
+                sb.append("&");
+            sb.append("scope=" + scopes[i]);
+        }
+        return sb.toString();
+    }
+}
