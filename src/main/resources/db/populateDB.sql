13f1a3eac69b8c3289209ebf82b852bfb4a98491
Brian Kelly and Ian Zabel <pair+bkelly+izabel@pivotallabs.com>
2013-10-11 16:56:03 -0400

    Assigns valid UUIDs to usage points for Alan.

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -18,8 +18,8 @@ INSERT INTO retail_customers (id, username, first_name, last_name, password, ena
 INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (7, 'marian',  'Marian',  'Rejewski',    'koala', 1, 'ROLE_USER');
 INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (8, 'grace',   'Grace',   'Hopper',      'koala', 1, 'ROLE_CUSTODIAN');
 
-INSERT INTO usage_points (id, description, serviceCategory_kind, retail_customer_id, uuid) VALUES (1, 'Front Electric Meter', 1, 1, '1');
-INSERT INTO usage_points (id, description, serviceCategory_kind, retail_customer_id, uuid) VALUES (2, 'Gas meter', 1, 1, '2');
+INSERT INTO usage_points (id, description, serviceCategory_kind, retail_customer_id, uuid) VALUES (1, 'Front Electric Meter', 1, 1, '7BC41774-7190-4864-841C-861AC76D46C2');
+INSERT INTO usage_points (id, description, serviceCategory_kind, retail_customer_id, uuid) VALUES (2, 'Gas meter', 1, 1, '7BC41774-7190-4864-841C-861AC76D46C3');
 INSERT INTO usage_points (id, description, serviceCategory_kind, retail_customer_id, uuid) VALUES (3, 'Front Electric Meter', 1, 2, '3');
 INSERT INTO usage_points (id, description, serviceCategory_kind, retail_customer_id, uuid) VALUES (4, 'Front Electric Meter', 1, 6, '4');
 INSERT INTO usage_points (id, description, serviceCategory_kind, retail_customer_id, uuid) VALUES (5, 'Gas meter', 1, 6, '5');
