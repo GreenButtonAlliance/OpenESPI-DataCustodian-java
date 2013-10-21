c44912929173f5b44e73edddd94b525db7ba12d7
Andy Pliszka and Ian Zabel <pair+andy+izabel@pivotallabs.com>
2013-10-21 15:38:30 -0400

    Enables XPath namespaces in XMLUnit test

--- src/main/java/org/energyos/espi/datacustodian/domain/Configuration.java
+++ src/main/java/org/energyos/espi/datacustodian/domain/Configuration.java
@@ -6,4 +6,9 @@ public class Configuration {
             "FB_4_5_15_IntervalDuration_3600_BlockDuration_monthly_HistoryLength_13",
             "FB_4_5_16_IntervalDuration_3600_BlockDuration_monthly_HistoryLength_13"
     };
+
+    public static final String HTTP_WWW_W3_ORG_2005_ATOM = "http://www.w3.org/2005/Atom";
+    public static final String HTTP_NAESB_ORG_ESPI = "http://naesb.org/espi";
+    public static final String ATOM_PREFIX = "";
+    public static final String ESPI_PREFIX = "espi";
 }
--- src/test/java/org/energyos/espi/datacustodian/atom/XMLTest.java
+++ src/test/java/org/energyos/espi/datacustodian/atom/XMLTest.java
@@ -1,13 +1,22 @@
 package org.energyos.espi.datacustodian.atom;
 
 
+import org.custommonkey.xmlunit.SimpleNamespaceContext;
 import org.custommonkey.xmlunit.XMLUnit;
 import org.junit.BeforeClass;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.energyos.espi.datacustodian.domain.Configuration.*;
 
 public class XMLTest {
 
     @BeforeClass
     public static void beforeClass() {
-        XMLUnit.getControlDocumentBuilderFactory().setNamespaceAware(false);
+        Map<String, String> m = new HashMap<>();
+        m.put(ATOM_PREFIX, HTTP_WWW_W3_ORG_2005_ATOM);
+        m.put(ESPI_PREFIX, HTTP_NAESB_ORG_ESPI);
+        XMLUnit.setXpathNamespaceContext(new SimpleNamespaceContext(m));
     }
 }
