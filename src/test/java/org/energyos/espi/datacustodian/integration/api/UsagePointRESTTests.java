15b1e9fd5a968e8fe067a69250c0c68234cb1381
Ian Zabel and Josh Clark <pair+izabel+jclark@pivotallabs.com>
2013-10-24 13:46:06 -0400

    Tests the structure of the UsagePoint API

--- src/test/java/org/energyos/espi/datacustodian/integration/api/UsagePointRESTTests.java
+++ src/test/java/org/energyos/espi/datacustodian/integration/api/UsagePointRESTTests.java
@@ -1,5 +1,10 @@
 package org.energyos.espi.datacustodian.integration.api;
 
+import org.energyos.espi.datacustodian.domain.ElectricPowerQualitySummary;
+import org.energyos.espi.datacustodian.domain.RetailCustomer;
+import org.energyos.espi.datacustodian.domain.UsagePoint;
+import org.energyos.espi.datacustodian.repositories.RetailCustomerRepository;
+import org.energyos.espi.datacustodian.repositories.UsagePointRepository;
 import org.energyos.espi.datacustodian.utils.TestUtils;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +17,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.energyos.espi.datacustodian.utils.factories.EspiFactory.newRetailCustomer;
+import static org.energyos.espi.datacustodian.utils.factories.EspiFactory.newUsagePoint;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
@@ -28,6 +35,12 @@ public class UsagePointRESTTests {
     @Autowired
     protected WebApplicationContext wac;
 
+    @Autowired
+    private RetailCustomerRepository retailCustomerRepository;
+
+    @Autowired
+    private UsagePointRepository usagePointRepository;
+
     @Before
     public void setup() {
         this.mockMvc = webAppContextSetup(this.wac).build();
@@ -38,5 +51,34 @@ public class UsagePointRESTTests {
         mockMvc.perform(get("/espi/1_1/resource/RetailCustomer/1/UsagePoint"))
             .andExpect(status().isOk())
             .andExpect(xpath("/:feed/:entry/:content/espi:UsagePoint", TestUtils.namespaces()).exists());
+    }
+
+    @Test
+    public void isOneLevelDeep() throws Exception {
+        RetailCustomer retailCustomer = newPersistedRetailCustomer();
+        Long id = retailCustomer.getId();
+        createUsagePoint(retailCustomer);
+
+
+        mockMvc.perform(get("/espi/1_1/resource/RetailCustomer/" + id + "/UsagePoint"))
+                .andExpect(status().isOk())
+                .andExpect(xpath("/:feed/:entry/:content/espi:UsagePoint", TestUtils.namespaces()).exists())
+                .andExpect(xpath("//espi:ElectricPowerQualitySummary", TestUtils.namespaces()).doesNotExist());
+    }
+
+    private RetailCustomer newPersistedRetailCustomer() {
+        RetailCustomer retailCustomer = newRetailCustomer();
+        retailCustomerRepository.persist(retailCustomer);
+        return retailCustomer;
+    }
+
+    private void createUsagePoint(RetailCustomer retailCustomer) {
+        UsagePoint usagePoint = newUsagePoint();
+        ElectricPowerQualitySummary e = new ElectricPowerQualitySummary();
+        e.setDescription("asdf");
+        usagePoint.getElectricPowerQualitySummaries().add(e);
+        usagePoint.setRetailCustomer(retailCustomer);
+
+        usagePointRepository.persist(usagePoint);
     }
 }
