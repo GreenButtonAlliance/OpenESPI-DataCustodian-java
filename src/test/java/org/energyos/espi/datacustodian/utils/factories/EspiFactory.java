e4e4dc1a995d1be95499c68129a18434bbc0b3e1
Ian Zabel and Josh Clark <pair+izabel+jclark@pivotallabs.com>
2013-10-24 13:38:48 -0400

    Truncates names for RetailCustomer to fit within JPA constraints

--- src/test/java/org/energyos/espi/datacustodian/utils/factories/EspiFactory.java
+++ src/test/java/org/energyos/espi/datacustodian/utils/factories/EspiFactory.java
@@ -86,8 +86,8 @@ public class EspiFactory {
 
     public static RetailCustomer newRetailCustomer() {
         RetailCustomer retailCustomer = new RetailCustomer();
-        retailCustomer.setFirstName("First" + System.currentTimeMillis());
-        retailCustomer.setLastName("Last" + System.currentTimeMillis());
+        retailCustomer.setFirstName(("First" + UUID.randomUUID()).substring(0, 29));
+        retailCustomer.setLastName(("Last" + UUID.randomUUID()).substring(0, 29));
 
         return retailCustomer;
     }
