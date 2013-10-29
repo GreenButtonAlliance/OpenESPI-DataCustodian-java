ec69f5905c03e3c0bfe575b46e45ecc02abdb35b
Ian Zabel and Joshua Clark <pair+izabel+jclark@pivotallabs.com>
2013-10-29 15:29:37 -0400

    Adds persist for AuthorizationService

--- src/main/java/org/energyos/espi/datacustodian/service/AuthorizationService.java
+++ src/main/java/org/energyos/espi/datacustodian/service/AuthorizationService.java
@@ -1,12 +1,10 @@
 package org.energyos.espi.datacustodian.service;
 
 import org.energyos.espi.datacustodian.domain.Authorization;
-import org.energyos.espi.datacustodian.domain.RetailCustomer;
 import org.energyos.espi.datacustodian.domain.Subscription;
-import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 public interface AuthorizationService {
     Authorization createAuthorization(Subscription subscription, String accessToken);
+
+    void persist(Authorization authorization);
 }
