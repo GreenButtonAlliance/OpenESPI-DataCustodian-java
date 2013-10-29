62788b45e0b3be9eb42ea776e69574968a752760
Brian Kelly <pair+bkelly@pivotallabs.com>
2013-10-29 11:45:11 -0400

    Updates URLs for ThirdParty ScopeSelection URL

--- src/main/resources/db/ec2-populateDB.sql
+++ src/main/resources/db/ec2-populateDB.sql
@@ -1,1 +1,1 @@
-INSERT INTO third_parties (id, client_id, name, url) VALUES(1, 'third_party', 'Third Party (EC2)', 'http://54.200.90.158:8080/ThirdParty/espi/1_1/RetailCustomer/ScopeSelectionList')
+INSERT INTO third_parties (id, client_id, name, url) VALUES(1, 'third_party', 'Third Party (EC2)', 'http://54.200.90.158:8080/ThirdParty/RetailCustomer/ScopeSelection')
