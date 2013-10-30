fbc5c0bd7cc18995cd22dccf25ce24376e0e0c9e
Brian Kelly and Ian Zabel <pair+bkelly+ian@pivotallabs.com>
2013-10-30 15:56:40 -0400

    Adds a NamespaceContext for the namespaces we are using.

--- src/test/java/org/energyos/espi/datacustodian/utils/TestUtils.java
+++ src/test/java/org/energyos/espi/datacustodian/utils/TestUtils.java
@@ -3,7 +3,9 @@ package org.energyos.espi.datacustodian.utils;
 import org.custommonkey.xmlunit.SimpleNamespaceContext;
 import org.custommonkey.xmlunit.XMLUnit;
 
+import javax.xml.namespace.NamespaceContext;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import static org.energyos.espi.datacustodian.domain.Configuration.*;
@@ -19,5 +21,24 @@ public class TestUtils {
         m.put(ATOM_PREFIX, HTTP_WWW_W3_ORG_2005_ATOM);
         m.put(ESPI_PREFIX, HTTP_NAESB_ORG_ESPI);
         return m;
+    }
+
+    public static NamespaceContext getNamespaceContext() {
+        return new NamespaceContext() {
+            @Override
+            public String getNamespaceURI(String prefix) {
+                return namespaces().get(prefix);
+            }
+
+            @Override
+            public String getPrefix(String namespaceURI) {
+                return null;
+            }
+
+            @Override
+            public Iterator getPrefixes(String namespaceURI) {
+                return null;
+            }
+        };
     }
 }
