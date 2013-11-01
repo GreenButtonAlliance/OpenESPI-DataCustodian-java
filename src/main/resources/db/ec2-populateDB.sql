e8d596b5a2485d8d33fd023f27ba7e3b4b66b441
Brian Kelly and Joshua Clark <pair+bkelly+jclark@pivotallabs.com>
2013-11-01 10:26:51 -0400

    Adds missing notification_uri to example third party in seed data.

--- src/main/resources/db/ec2-populateDB.sql
+++ src/main/resources/db/ec2-populateDB.sql
@@ -1,1 +1,1 @@
-INSERT INTO third_parties (id, client_id, name, url) VALUES(1, 'third_party', 'Third Party (EC2)', 'http://54.200.90.158:8080/ThirdParty/RetailCustomer/ScopeSelection')
+INSERT INTO third_parties (id, client_id, name, url, notification_uri) VALUES(1, 'third_party', 'Third Party (EC2)', 'http://54.200.90.158:8080/ThirdParty/RetailCustomer/ScopeSelection', 'http://localhost:8080/ThirdParty/espi/1_1/Notification')
