30ee09f2635bd0b6f80b659b22a9d1d7ca5d24ca
Brian Kelly and Joshua Clark <pair+bkelly+jclark@pivotallabs.com>
2013-11-07 14:33:31 -0500

    Modifies scopes to conform to new format

--- src/main/java/org/energyos/espi/datacustodian/domain/Configuration.java
+++ src/main/java/org/energyos/espi/datacustodian/domain/Configuration.java
@@ -3,8 +3,8 @@ package org.energyos.espi.datacustodian.domain;
 public class Configuration {
     public static final String DATA_CUSTODIAN_ID = "data_custodian";
     public static final String[] SCOPES = new String [] {
-            "FB_4_5_15_IntervalDuration_3600_BlockDuration_monthly_HistoryLength_13",
-            "FB_4_5_16_IntervalDuration_3600_BlockDuration_monthly_HistoryLength_13"
+            "FB=4_5_15;IntervalDuration=3600;BlockDuration=monthly;HistoryLength=13",
+            "FB=4_5_16;IntervalDuration=3600;BlockDuration=monthly;HistoryLength=13"
     };
 
     public static final String HTTP_WWW_W3_ORG_2005_ATOM = "http://www.w3.org/2005/Atom";
