524ae4eb6107b550799324b4e21804d069c6f62a
Brian Kelly and Joshua Clark <pair+bkelly+jclark@pivotallabs.com>
2013-11-11 16:28:06 -0500

    Fixes install to cd into checked out directory

--- bin/install_common.sh
+++ bin/install_common.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
-git clone git://github.com/energyos/OpenESPI-Common-java.git
+git clone git://github.com/energyos/OpenESPI-Common-java.git common
+pushd common
 mvn clean install
+popd
