a60ab6f8d27adfda6412a16e17d64e2bcc3b420c
Andy Pliszka and Ian Zabel <pair+apliszka+ian@pivotallabs.com>
2013-11-20 17:19:14 -0500

    Refactors ThirdPartyRepository into ApplicationInformationRepository

--- src/test/java/org/energyos/espi/datacustodian/repositories/jpa/UsagePointRepositoryImplTests.java
+++ src/test/java/org/energyos/espi/datacustodian/repositories/jpa/UsagePointRepositoryImplTests.java
@@ -19,9 +19,9 @@ package org.energyos.espi.datacustodian.repositories.jpa;
 
 import org.energyos.espi.common.domain.*;
 import org.energyos.espi.common.models.atom.LinkType;
+import org.energyos.espi.common.repositories.ApplicationInformationRepository;
 import org.energyos.espi.common.repositories.RetailCustomerRepository;
 import org.energyos.espi.common.repositories.SubscriptionRepository;
-import org.energyos.espi.common.repositories.ThirdPartyRepository;
 import org.energyos.espi.common.repositories.UsagePointRepository;
 import org.energyos.espi.common.test.EspiPersistenceFactory;
 import org.energyos.espi.datacustodian.utils.factories.EspiFactory;
@@ -52,7 +52,7 @@ public class UsagePointRepositoryImplTests {
     @Autowired
     RetailCustomerRepository retailCustomerRepository;
     @Autowired
-    ThirdPartyRepository thirdPartyRepository;
+    ApplicationInformationRepository applicationInformationRepository;
     @Autowired
     private SubscriptionRepository subscriptionRepository;
     @Autowired
@@ -354,7 +354,7 @@ public class UsagePointRepositoryImplTests {
         repository.persist(updatedUsagePoint);
 
         ThirdParty thirdParty = EspiFactory.newThirdParty();
-        thirdPartyRepository.persist(thirdParty);
+        applicationInformationRepository.persist(thirdParty);
 
         Subscription subscription = newSubscription(retailCustomer, thirdParty);
         subscription.setLastUpdate(EspiFactory.newCalendar(2013, 11, 15));
