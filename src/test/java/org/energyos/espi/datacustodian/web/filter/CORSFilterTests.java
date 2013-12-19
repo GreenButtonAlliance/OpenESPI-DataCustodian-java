9c531cd89e124a819dcfb4f43dfc46e51cd557a6
Donald F Coffin <donald.coffin@reminetworks.com>
2013-12-18 21:02:23 -0800

    Move CORSFilter.java to OpenESPI-Common-java Remove OriginFilter.java Remove OriginFilterTests.java Modified web.xml to find CORSFilter in OpenESPI-Common-java

--- src/test/java/org/energyos/espi/datacustodian/web/filter/CORSFilterTests.java
+++ src/test/java/org/energyos/espi/datacustodian/web/filter/CORSFilterTests.java
@@ -1,5 +1,6 @@
 package org.energyos.espi.datacustodian.web.filter;
 
+import org.energyos.espi.common.utils.CORSFilter;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
