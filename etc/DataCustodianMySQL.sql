23c7d45b23191b4ab95fc5254a5972a388f831cd
Marty Burns <marty@hypertek.us>
2013-12-30 18:58:31 -0500

    modified pom to allow no database create/drop on profiles basis

--- etc/DataCustodianMySQL.sql
+++ etc/DataCustodianMySQL.sql
@@ -532,7 +532,7 @@ DROP TABLE IF EXISTS `retail_customers`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `retail_customers` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `enabled` tinyint(1) NOT NULL,
+  `enabled` boolean NOT NULL,
   `first_name` varchar(30) NOT NULL,
   `last_name` varchar(30) NOT NULL,
   `password` varchar(100) DEFAULT NULL,
