5354f6fa327b141625b66da16d6db85f52d8244d
Donald F Coffin <donald.coffin@reminetworks.com>
2014-02-10 11:00:54 -0800

    WIP

--- src/main/java/org/energyos/espi/datacustodian/web/custodian/AdminController.java
+++ src/main/java/org/energyos/espi/datacustodian/web/custodian/AdminController.java
@@ -29,18 +29,18 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @PreAuthorize("hasRole('ROLE_CUSTODIAN')")
 public class AdminController {
 
-//    @Autowired
+    @Autowired
     private ConsumerTokenServices tokenServices;
     
     private TokenStore tokenStore;
     
-//    @Autowired
+    @Autowired
     private SubscriptionService subscriptionService;
     
-//    @Autowired
+    @Autowired
     private NotificationService notificationService;
 
-	private EspiUserApprovalHandler userApprovalHandler;
+//	private EspiUserApprovalHandler userApprovalHandler;
 
     @RequestMapping(value = Routes.DATA_CUSTODIAN_NOTIFY_THIRD_PARTY, method = RequestMethod.GET)
     public String notifyThirdParty() throws Exception {
@@ -66,71 +66,71 @@ public class AdminController {
     //*
     //**************************************************************************************************
 
-	@RequestMapping("/oauth/cache_approvals")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void startCaching() throws Exception {
-		userApprovalHandler.setUseApprovalStore(true);
-	}
-
-	@RequestMapping("/oauth/uncache_approvals")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void stopCaching() throws Exception {
-		userApprovalHandler.setUseApprovalStore(false);
-	}
-
-	@RequestMapping("/oauth/users/{user}/tokens")
-	@ResponseBody
-	public Collection<OAuth2AccessToken> listTokensForUser(@PathVariable String user, Principal principal)
-			throws Exception {
-		checkResourceOwner(user, principal);
-		return tokenStore.findTokensByUserName(user);
-	}
-
-	@RequestMapping(value = "/oauth/users/{user}/tokens/{token}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void> revokeToken(@PathVariable String user, @PathVariable String token, Principal principal)
-			throws Exception {
-		checkResourceOwner(user, principal);
-		if (tokenServices.revokeToken(token)) {
-			return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
-		} else {
-			return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);			
-		}
-	}
-
-	@RequestMapping("/oauth/clients/{client}/tokens")
-	@ResponseBody
-	public Collection<OAuth2AccessToken> listTokensForClient(@PathVariable String client) throws Exception {
-		return tokenStore.findTokensByClientId(client);
-	}
-
-	private void checkResourceOwner(String user, Principal principal) {
-		if (principal instanceof OAuth2Authentication) {
-			OAuth2Authentication authentication = (OAuth2Authentication) principal;
-			if (!authentication.isClientOnly() && !user.equals(principal.getName())) {
-				throw new AccessDeniedException(String.format("User '%s' cannot obtain tokens for user '%s'",
-						principal.getName(), user));
-			}
-		}
-	}
-
-	/**
-	 * @param userApprovalHandler the userApprovalHandler to set
-	 */
-	public void setUserApprovalHandler(EspiUserApprovalHandler userApprovalHandler) {
-		this.userApprovalHandler = userApprovalHandler;
-	}
-
-	/**
-	 * @param tokenServices the consumerTokenServices to set
-	 */
-	public void setTokenServices(ConsumerTokenServices tokenServices) {
-		this.tokenServices = tokenServices;
-	}
-	
-	/**
-	 * @param tokenStore the tokenStore to set
-	 */
-	public void setTokenStore(TokenStore tokenStore) {
-		this.tokenStore = tokenStore;
-	}    
+//	@RequestMapping("/oauth/cache_approvals")
+//	@ResponseStatus(HttpStatus.NO_CONTENT)
+//	public void startCaching() throws Exception {
+//		userApprovalHandler.setUseApprovalStore(true);
+//	}
+//
+//	@RequestMapping("/oauth/uncache_approvals")
+//	@ResponseStatus(HttpStatus.NO_CONTENT)
+//	public void stopCaching() throws Exception {
+//		userApprovalHandler.setUseApprovalStore(false);
+//	}
+//
+//	@RequestMapping("/oauth/users/{user}/tokens")
+//	@ResponseBody
+//	public Collection<OAuth2AccessToken> listTokensForUser(@PathVariable String user, Principal principal)
+//			throws Exception {
+//		checkResourceOwner(user, principal);
+//		return tokenStore.findTokensByUserName(user);
+//	}
+//
+//	@RequestMapping(value = "/oauth/users/{user}/tokens/{token}", method = RequestMethod.DELETE)
+//	public ResponseEntity<Void> revokeToken(@PathVariable String user, @PathVariable String token, Principal principal)
+//			throws Exception {
+//		checkResourceOwner(user, principal);
+//		if (tokenServices.revokeToken(token)) {
+//			return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+//		} else {
+//			return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);			
+//		}
+//	}
+//
+//	@RequestMapping("/oauth/clients/{client}/tokens")
+//	@ResponseBody
+//	public Collection<OAuth2AccessToken> listTokensForClient(@PathVariable String client) throws Exception {
+//		return tokenStore.findTokensByClientId(client);
+//	}
+//
+//	private void checkResourceOwner(String user, Principal principal) {
+//		if (principal instanceof OAuth2Authentication) {
+//			OAuth2Authentication authentication = (OAuth2Authentication) principal;
+//			if (!authentication.isClientOnly() && !user.equals(principal.getName())) {
+//				throw new AccessDeniedException(String.format("User '%s' cannot obtain tokens for user '%s'",
+//						principal.getName(), user));
+//			}
+//		}
+//	}
+//
+//	/**
+//	 * @param userApprovalHandler the userApprovalHandler to set
+//	 */
+//	public void setUserApprovalHandler(EspiUserApprovalHandler userApprovalHandler) {
+//		this.userApprovalHandler = userApprovalHandler;
+//	}
+//
+//	/**
+//	 * @param tokenServices the consumerTokenServices to set
+//	 */
+//	public void setTokenServices(ConsumerTokenServices tokenServices) {
+//		this.tokenServices = tokenServices;
+//	}
+//	
+//	/**
+//	 * @param tokenStore the tokenStore to set
+//	 */
+//	public void setTokenStore(TokenStore tokenStore) {
+//		this.tokenStore = tokenStore;
+//	}    
 }
