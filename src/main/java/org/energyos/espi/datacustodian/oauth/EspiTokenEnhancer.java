d4ccb2c30afdff52f6a406aa545626af8d050b4a
Donald F Coffin <donald.coffin@reminetworks.com>
2014-02-28 22:04:30 -0800

    - Modified Access Token Enhancer to test for ClientOnly (i.e. "client_credentials request) based access tokens and bypass creation of DataCustodian authorization and subscription table entries.  There is no authentication structure created by Spring Security Oauth framework for client application created access tokens, thus the current logic fails with an EmptyDatasetException

--- src/main/java/org/energyos/espi/datacustodian/oauth/EspiTokenEnhancer.java
+++ src/main/java/org/energyos/espi/datacustodian/oauth/EspiTokenEnhancer.java
@@ -46,49 +46,55 @@ public class EspiTokenEnhancer implements TokenEnhancer {
     	
 		DefaultOAuth2AccessToken result = new DefaultOAuth2AccessToken(accessToken);
 		result.setAdditionalInformation(Collections.singletonMap("client_id", (Object) authentication.getOAuth2Request().getClientId()));	
-		
-        // Create Subscription and add resourceURI to /oath/token response
-        Subscription subscription = subscriptionService.createSubscription(authentication);   
-        result.getAdditionalInformation().put("resourceURI", baseURL + Routes.BATCH_SUBSCRIPTION.replace("{subscriptionId}", subscription.getId().toString()));        
 
-        // Create Authorization and add authorizationURI to /oath/token response        
-        Authorization authorization = authorizationService.createAuthorization(subscription, result.getValue());           
-        result.getAdditionalInformation().put("authorizationURI", baseURL + Routes.DATA_CUSTODIAN_AUTHORIZATION.replace("{AuthorizationID}", authorization.getId().toString()));        	
-        
-        subscription.setAuthorization(authorization);
-		subscription.setUpdated(new GregorianCalendar());        
-        subscriptionService.merge(subscription);
+		//TODO: Change subscription and authorization table entry creation to work for "client_credentials" access token request
+		// Is this access token for an application (i.e. "client_credentials" request?
+		if (authentication.isClientOnly() == false){  // No, continue
+			
+			// Create Subscription and add resourceURI to /oath/token response
+			Subscription subscription = subscriptionService.createSubscription(authentication);   
+			result.getAdditionalInformation().put("resourceURI", baseURL + Routes.BATCH_SUBSCRIPTION.replace("{subscriptionId}", subscription.getId().toString()));        
 
-		RetailCustomer retailCustomer = (RetailCustomer) authentication.getPrincipal();	
-		
-        // link in the usage points associated with this subscription
-        List<Long> usagePointIds = resourceService.findAllIdsByXPath(retailCustomer.getId(), UsagePoint.class);
-        Iterator<Long> it = usagePointIds.iterator();
-        while (it.hasNext()) {
-        	UsagePoint up = resourceService.findById(it.next(), UsagePoint.class);
-        	up.setSubscription(subscription);
-        	resourceService.persist(up);  // maybe not needed??
-        }
+			// Create Authorization and add authorizationURI to /oath/token response        
+			Authorization authorization = authorizationService.createAuthorization(subscription, result.getValue());           
+			result.getAdditionalInformation().put("authorizationURI", baseURL + Routes.DATA_CUSTODIAN_AUTHORIZATION.replace("{AuthorizationID}", authorization.getId().toString()));        	
         
-        authorization.setApplicationInformation(applicationInformationService.findByClientId(authentication.getOAuth2Request().getClientId()));
-        authorization.setThirdParty(authentication.getOAuth2Request().getClientId());
-        authorization.setRetailCustomer(retailCustomer);
-        authorization.setAccessToken(accessToken.getValue());        
-        authorization.setTokenType(accessToken.getTokenType());
-        authorization.setExpiresIn((long) accessToken.getExpiresIn());
-        
-		if(accessToken.getRefreshToken() != null) {
-			authorization.setRefreshToken(accessToken.getRefreshToken().toString());			
-		}
+			subscription.setAuthorization(authorization);
+			subscription.setUpdated(new GregorianCalendar());        
+			subscriptionService.merge(subscription);
+
+			RetailCustomer retailCustomer = (RetailCustomer) authentication.getPrincipal();	
 		
-		// Remove "[" and "]" surrounding Scope in accessToken structure
-        authorization.setScope(accessToken.getScope().toString().substring(1, (accessToken.getScope().toString().length()-1)));
-        authorization.setAuthorizationURI(baseURL + Routes.DATA_CUSTODIAN_AUTHORIZATION.replace("{AuthorizationID}", authorization.getId().toString()));
-        authorization.setResourceURI(baseURL + Routes.BATCH_SUBSCRIPTION.replace("{subscriptionId}", subscription.getId().toString()));
-		authorization.setUpdated(new GregorianCalendar());
-		authorization.setStatus("1"); 	// Set authorization record status as "Active"
-		authorization.setSubscription(subscription);
-        authorizationService.merge(authorization);
+			// link in the usage points associated with this subscription
+			List<Long> usagePointIds = resourceService.findAllIdsByXPath(retailCustomer.getId(), UsagePoint.class);
+			Iterator<Long> it = usagePointIds.iterator();
+			while (it.hasNext()) {
+				UsagePoint up = resourceService.findById(it.next(), UsagePoint.class);
+				up.setSubscription(subscription);
+				resourceService.persist(up);  // maybe not needed??
+			}
+			
+        
+			authorization.setApplicationInformation(applicationInformationService.findByClientId(authentication.getOAuth2Request().getClientId()));
+			authorization.setThirdParty(authentication.getOAuth2Request().getClientId());
+			authorization.setRetailCustomer(retailCustomer);
+			authorization.setAccessToken(accessToken.getValue());        
+			authorization.setTokenType(accessToken.getTokenType());
+			authorization.setExpiresIn((long) accessToken.getExpiresIn());
+        
+			if(accessToken.getRefreshToken() != null) {
+				authorization.setRefreshToken(accessToken.getRefreshToken().toString());			
+			}
+		
+			// Remove "[" and "]" surrounding Scope in accessToken structure
+			authorization.setScope(accessToken.getScope().toString().substring(1, (accessToken.getScope().toString().length()-1)));
+			authorization.setAuthorizationURI(baseURL + Routes.DATA_CUSTODIAN_AUTHORIZATION.replace("{AuthorizationID}", authorization.getId().toString()));
+			authorization.setResourceURI(baseURL + Routes.BATCH_SUBSCRIPTION.replace("{subscriptionId}", subscription.getId().toString()));
+			authorization.setUpdated(new GregorianCalendar());
+			authorization.setStatus("1"); 	// Set authorization record status as "Active"
+			authorization.setSubscription(subscription);
+			authorizationService.merge(authorization);
+		}			
 
         return result;
     }
