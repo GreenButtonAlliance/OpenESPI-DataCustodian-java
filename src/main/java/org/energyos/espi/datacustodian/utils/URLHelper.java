52f92c8270c8be7840c7fe73ece025fb92ac34e3
Donald F Coffin <donald.coffin@reminetworks.com>
2014-03-04 23:36:42 -0800

    Correct Scope String build routine to separate SCOPE values by a space rather than additional query parameters

--- src/main/java/org/energyos/espi/datacustodian/utils/URLHelper.java
+++ src/main/java/org/energyos/espi/datacustodian/utils/URLHelper.java
@@ -4,23 +4,27 @@ import java.util.Set;
 
 public class URLHelper {
 
-    public static String newScopeParams(String[] scopes) {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < scopes.length; i++) {
-            if(i > 0)
-                sb.append("&");
-            sb.append("scope=" + scopes[i]);
-        }
-        return sb.toString();
-    }
+//    public static String newScopeParams(String[] scopes) {
+//        StringBuilder sb = new StringBuilder();
+//        for(int i = 0; i < scopes.length; i++) {
+//            if(i == 0) {
+//                sb.append("scope=" + scopes[i]);
+//            } else {
+//            	sb.append(" " + scopes[i]);
+//            }
+//        }
+//        return sb.toString();
+//    }
 
     public static String newScopeParams(Set<String> scopes) {
         StringBuilder sb = new StringBuilder();
         int i = 0;
         for(String scope : scopes) {
-            if(i > 0)
-                sb.append("&");
-            sb.append("scope=" + scope);
+            if(i == 0) {
+                sb.append("scope=" + scope);
+            } else {
+            	sb.append(" " + scope);
+            }
             i++;
         }
         return sb.toString();
