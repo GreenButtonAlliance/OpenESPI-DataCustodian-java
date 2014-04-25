c0df56a13b86e1177856d7adbff3dd077f573f9b
MartyBurns <marty@hypertek.us>
2014-04-25 16:30:12 -0400

    commented out bad line of code due to OAuth library change; non-used function

--- src/main/java/org/energyos/espi/datacustodian/oauth/OauthAdminController.java
+++ src/main/java/org/energyos/espi/datacustodian/oauth/OauthAdminController.java
@@ -108,7 +108,8 @@ public class OauthAdminController extends BaseController {
 	public Collection<OAuth2AccessToken> listTokensForUser(@PathVariable String userID, Principal principal)
 			throws Exception {
 		checkResourceOwner(userID, principal);
-		return tokenStore.findTokensByUserName(userID);
+//		return tokenStore.findTokensByUserName(userID);
+	return null;
 	}
 
 	/**
