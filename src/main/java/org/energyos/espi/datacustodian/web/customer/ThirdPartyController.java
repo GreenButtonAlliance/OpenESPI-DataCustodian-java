2e10e3d487281f3ac10120674135bb357cded5dd
jateeter <john.teeter@energyos.org>
2014-08-06 18:28:08 -0400

    focus on ApplicationInformationService. Clean-up and Documentation

--- src/main/java/org/energyos/espi/datacustodian/web/customer/ThirdPartyController.java
+++ src/main/java/org/energyos/espi/datacustodian/web/customer/ThirdPartyController.java
@@ -3,6 +3,7 @@ package org.energyos.espi.datacustodian.web.customer;
 import org.energyos.espi.common.domain.ApplicationInformation;
 import org.energyos.espi.common.domain.Routes;
 import org.energyos.espi.common.service.ApplicationInformationService;
+import org.energyos.espi.common.service.ResourceService;
 import org.energyos.espi.datacustodian.utils.URLHelper;
 import org.energyos.espi.datacustodian.web.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,17 +20,20 @@ public class ThirdPartyController extends BaseController {
 
     @Autowired
     private ApplicationInformationService applicationInformationService;
+    
+    @Autowired
+    private ResourceService resourceService;
 
     @RequestMapping(method = RequestMethod.GET)
     public String index(ModelMap model) {
+    	// note that we are only looking at "THIRD_PARTY" relationships here.
         model.put("applicationInformationList", applicationInformationService.findByKind("THIRD_PARTY"));
-//        model.put("applicationInformationList", applicationInformationService.findAll());
         return "/customer/thirdparties/index";
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public String selectThirdParty(@RequestParam("Third_party") Long thirdPartyId, @RequestParam("Third_party_URL") String thirdPartyURL) {
-        ApplicationInformation applicationInformation = applicationInformationService.findById(thirdPartyId);
+        ApplicationInformation applicationInformation = resourceService.findById(thirdPartyId, ApplicationInformation.class);
         return "redirect:" + thirdPartyURL + "?" + URLHelper.newScopeParams(applicationInformation.getScope()) 
         		+ "&DataCustodianID=" + applicationInformation.getDataCustodianId();
     }
