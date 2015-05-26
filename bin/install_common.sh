7557bb2754d2ac6c4a0e6903ae8da9eeb678d4b7
Donald F Coffin <donald.coffin@reminetworks.com>
2015-05-26 13:00:10 -0400

    Change URL for OpenESPI-Common-java to use https:// prefix

--- bin/install_common.sh
+++ bin/install_common.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-git clone git://github.com/energyos/OpenESPI-Common-java.git common
+git clone https://github.com/energyos/OpenESPI-Common-java.git common
 cd common
 mvn clean install
 cd ..
